24a2c2d89c0517769464a0c3342cbb51002a4933
Ches Chesy <chesy47@gmail.com>
2020-02-18 14:11:17 +0200

    Add Header and footer Adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -33,8 +33,30 @@ $(function() {
         $('.icon-list').removeClass('active');
     });
     
+    $('.menu__btn').on('click', function() {
+       $('.menu__list').slideToggle();       
+    });
+    
+     $('.header__btn-menu').on('click', function() {
+        $('.header__box').toggleClass('active'); 
+    });
+    
     
     
     var mixer = mixitup('.products__inner-box');
     
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
